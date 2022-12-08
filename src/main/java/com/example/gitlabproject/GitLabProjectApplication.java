48a31f93bb29fbc08310fbf16e3108c2284b91c3
pi koo <rnrudejr1234@naver.com>
2022-12-08 14:28:35 +0900

    first commit

---
+++ src/main/java/com/example/gitlabproject/GitLabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLabProjectApplication.class, args);
+    }
+
+}
